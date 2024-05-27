c60061a67d42c34909bfa066d99cc852fdc41b37
Bijoy99roy <bijoy2014amp@gmail.com>
2024-05-27 23:59:02 +0530

    chore: Modify payload for queue

--- src/routes/uploadData.ts
+++ src/routes/uploadData.ts
@@ -2,6 +2,7 @@ import express from "express";
 import upload from "../middleware/uploadMiddleware";
 import {createClient} from "redis";
 import redisConfig from "../config";
+import { v4 as uuidv4 } from "uuid";
 
 const uploadRouter = express.Router();
 
@@ -16,10 +17,25 @@ redisClient.on("connect", () => {
 })
 
 uploadRouter.post("/uploadfile", upload.single("videoFile"), async (req, res) => {
-    console.log(req.file)
+    // console.log(req.file)
     
     try{
-        await redisClient.rPush(redisConfig.queueName, JSON.stringify(req.file))
+        const videoId = uuidv4();
+        let payload = {
+            fieldname: 'videoFile',
+            originalname: 'lake.mp4',
+            encoding: '7bit',
+            mimetype: 'video/mp4',
+            destination: './uploads',
+            filename: 'a75094e0-d685-49be-8f36-b84db1b45329.mp4',
+            videoPath: `E:\\Projects\\video_transcoder_backend_api\\uploads\\${req.file?.filename}`,
+            size: 42992636,
+            outputPath:  `./uploads/hls-videos/${videoId}`,
+            videoId: videoId,
+            retries: 0
+        }
+        console.log(payload)
+        await redisClient.rPush(redisConfig.queueName, JSON.stringify(payload))
         res.status(200).json({
             message: "File added to the processing queue"
         })
